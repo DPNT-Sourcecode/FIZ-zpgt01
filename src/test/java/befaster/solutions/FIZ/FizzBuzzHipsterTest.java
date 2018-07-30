050842ce3104811b12620ee49a2df7ac96d3e3ba
sbx_user1061 <sbx_user1061@ip-10-12-185-83.eu-west-2.compute.internal>
2018-07-30 13:54:13 +0000

    Mon Jul 30 13:54:13 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzHipsterTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzHipsterTest.java
@@ -16,12 +16,12 @@ public class FizzBuzzHipsterTest {
     }
 
     /**
-     *  The old rules for "deluxe" do not apply.
-     *   - A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
-     *   - A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
-     *   - We should still say "fake deluxe" if the "deluxe" number is odd
-     *   - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
-     *   - All the previous rules for "fizz" and "buzz" are still valid
+     * The old rules for "deluxe" do not apply.
+     * - A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
+     * - A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
+     * - We should still say "fake deluxe" if the "deluxe" number is odd
+     * - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
+     * - All the previous rules for "fizz" and "buzz" are still valid
      */
     @Test
     public void fizzBuzz_3_and_3() {
@@ -29,4 +29,15 @@ public class FizzBuzzHipsterTest {
         assertThat(fizzBuzzSolution.fizzBuzz(33), is(FizzBuzzSolution.FIZZ_FAKE_DELUXE));
     }
 
+    @Test
+    public void fizzBuzz_divides_by_5_and_contains_5() {
+        //but no repeated digits so vanilla
+        assertThat(fizzBuzzSolution.fizzBuzz(25), is(FizzBuzzSolution.BUZZ));
+    }
+
+    @Test
+    public void fizzBuzz_divides_by_5_and_contains_5_multi() {
+        //but no repeated digits so vanilla
+        assertThat(fizzBuzzSolution.fizzBuzz(55), is(FizzBuzzSolution.BUZZ_FAKE_DELUXE));
+    }
 }
