0c36b093abb21348337d95312a6214ce98418689
sbx_user1061 <sbx_user1061@ip-10-12-185-83.eu-west-2.compute.internal>
2018-07-30 12:54:12 +0000

    Mon Jul 30 12:54:12 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,7 +2,28 @@ package befaster.solutions.FIZ;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.function.IntFunction;
+
 public class FizzBuzzSolution {
+    public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
+    public static final String FIZZ_BUZZ = FIZZ + " " + BUZZ;
+
+    /**
+     * If the number is a multiple of three then you should write "fizz"
+     * - If the number is a multiple of five then you should write "buzz"
+     * - If the number is a multiple of both three and five then you should write "fizz buzz"
+     * - If the number is not a multiple of five or three then write the number, example 1
+     */
+    private final IntFunction<String> FIZZ_BUZZ_IMPL = new IntFunction<String>() {
+        @Override
+        public String apply(int value) {
+            if(value %3 == 0 && value % 5 == 0){
+
+            }
+            return null;
+        }
+    };
 
     public String fizzBuzz(Integer number) {
         throw new SolutionNotImplementedException();
