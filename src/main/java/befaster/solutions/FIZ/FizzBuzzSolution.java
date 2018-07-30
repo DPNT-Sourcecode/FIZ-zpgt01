e014c6530437561f02a9e6475bafb2343ecdf385
sbx_user1061 <sbx_user1061@ip-10-12-185-83.eu-west-2.compute.internal>
2018-07-30 13:06:33 +0000

    Mon Jul 30 13:06:33 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -14,7 +14,7 @@ public final class FizzBuzzSolution {
      * - If the number is not a multiple of five or three then write the number, example 1
      */
     private final IntFunction<String> FIZZ_BUZZ_IMPL = value -> {
-        if(value == 0){
+        if (value == 0) {
             return Integer.toString(value);
         }
         if (value % 3 == 0 && value % 5 == 0) {
@@ -29,26 +29,38 @@ public final class FizzBuzzSolution {
 
 
     /**
-     * 
+     * - A number is "fizz" if it is divisible by 3 or if it has a 3 in it
+     * - A number is "buzz" if it is divisible by 5 or if it has a 5 in it
+     * - A number can be both "fizz" and "buzz" at the same time.
+     * If this happens then write "fizz buzz"
      */
     private final IntFunction<String> FIZZ_BUZZ_IMPL_V2 = value -> {
-        if(value == 0){
+        if (value == 0) {
             return Integer.toString(value);
         }
-        if (value % 3 == 0 && value % 5 == 0) {
-            return FIZZ_BUZZ;
-        } else if (value % 3 == 0) {
-            return FIZZ;
-        } else if (value % 5 == 0) {
-            return BUZZ;
+        String result = "";
+        String number = Integer.toString(value);
+        if (value % 3 == 0 || number.contains("3")) {
+            result = FIZZ;
         }
-        return Integer.toString(value);
+        if (value % 5 == 0 || number.contains("5")) {
+            if (result.isEmpty()) {
+                result = BUZZ;
+            } else {
+                result = FIZZ_BUZZ;
+            }
+        }
+        if(result.isEmpty()){
+            result = number''
+        }
+        return result;
     };
 
     /**
      * Notes:
-     *  - You do not have to test for illegal input.
-     *  - The input numbers are between 1-9999
+     * - You do not have to test for illegal input.
+     * - The input numbers are between 1-9999
+     *
      * @param number
      * @return
      */
